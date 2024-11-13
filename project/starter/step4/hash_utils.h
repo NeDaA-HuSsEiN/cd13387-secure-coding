b9108232996291917980ea79d0f0c610717c15a8
Nedaa Hussein <nedaa.hussein@thirdwayv.com>
2024-11-13 20:30:14 +0200

    Minor change

--- project/starter/step4/hash_utils.h
+++ project/starter/step4/hash_utils.h
@@ -4,5 +4,5 @@
 void bytes_to_hex(const unsigned char* bytes, size_t len, char* hex_str);
 void hash_password(const char* password, const unsigned char* salt, char* hashed_password);
 void generate_salt(unsigned char* salt, size_t length);
-void hexStringToBytes(const char *hex_str, unsigned char *bytes);
+void hex_to_bytes(const char* hex_str, size_t len, unsigned char* bytes);
 #endif
